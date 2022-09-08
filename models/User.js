05f35619c5ad481921c2198c5f866dbfedf70bb2
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-09-07 20:50:01 -0700

    Added User model and npm scripts

---
+++ models/User.js
@@ -0,0 +1,62 @@
+const { Model, DataTypes } = require( 'sequelize' );
+const bcrypt = require( 'bcrypt' );
+const sequelize = require( '../config/connection' );
+
+class User extends Model {
+    checkPassword ( loginPassword ) {
+        return bcrypt.compareSync( loginPassword, this.password );
+    }
+}
+
+User.init( 
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        name: {
+            type: DataTypes.STRING( 50 ),
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING( 30 ),
+            allowNull: false,
+            unique: true,
+            validate: {
+                isAlphanumeric: true
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+              isEmail: true,
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
+        }
+    },
+    {
+        hooks: {
+            async beforeCreate( newUserData ) {
+                newUserData.password = await bcrypt.hash( newUserData.password, 10 );
+                return newUserData;
+            }
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+ );
+
+ module.exports = User;
