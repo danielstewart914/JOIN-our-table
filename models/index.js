84d8244cf9fd6ee5490f4632cb4773972f3ed7de
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-09-09 10:05:56 -0700

    Added model relations in models/index.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,49 @@
 const User = require( './User' );
+const Ingredient = require( './Ingredient' );
+const Recipe = require( './Recipe' );
+const RecipeIngredient = require( './RecipeIngredient' );
+const Unit = require( './Unit' );
 
+User.hasMany( Recipe, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+} );
 
+Recipe.belongsTo( User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+} );
 
-module.exports = { User };
+Recipe.belongsToMany( Ingredient, {
+    through: {
+        model: RecipeIngredient,
+        unique: false
+    },
+    as: 'ingredients'
+} );
+
+Ingredient.belongsToMany( Recipe, {
+    through: {
+        model: RecipeIngredient,
+        unique: false
+    },
+    as: 'recipes'
+} );
+
+Unit.belongsToMany( Recipe, {
+    through: {
+        model: RecipeIngredient,
+        unique: false
+    },
+    as: 'recipes'
+} );
+
+Recipe.belongsToMany( Unit, {
+    through: {
+        model: RecipeIngredient,
+        unique: false
+    },
+    as: 'unit'
+} );
+
+module.exports = { User, Ingredient, Recipe, RecipeIngredient, Unit };
