b86a45990b011eefedb15514290fc95058df7838
azm89 <ashtonmoore89@gmail.com>
2022-09-10 12:04:00 -0700

    added 404 message to get by id route

--- controllers/api/unitRoutes.js
+++ controllers/api/unitRoutes.js
@@ -15,6 +15,10 @@ unitRouter.get('/units', async (req, res) => {
 unitRouter.get('/units/:id', async (req, res) => {
     try {
         const unitData = await Unit.findByPk(req.params.id);
+        if (!unitData) {
+            res.status(404).json({ message: 'No unit found with that id!' });
+            return;
+        }
         res.status(200).json(unitData);
     } catch (err) {
         res.status(500).json(err);
