af7a92da93fb91409a768a0091a2d9b9c890fd0e
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-10 13:18:17 -0700

    adding POST route to ingredientsRoutes

--- controllers/api/ingredientsRoutes.js
+++ controllers/api/ingredientsRoutes.js
@@ -27,3 +27,13 @@ ingredientsRouter.get('/:id', async (req, res) => {
       res.status(500).json(err);
     }
   });
+ 
+  // POST a new ingredient 
+ ingredientsRouter.post('/ingredients', async (req, res) => {
+    try {
+        const ingredientsData = await Ingredient.create(req.body);
+        res.status(200).json(ingredientsData);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
