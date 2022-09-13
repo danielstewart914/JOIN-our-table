d8e033f9bad4ce0844a9deee8ee2dcd772c8edc3
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-12 18:16:32 -0700

    added an extra comma that crashed the app. whoops

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,5 +5,5 @@ const ingredientsRoutes= require('./ingredientsRoutes')
 
 apiRouter.use('/users', userRoutes);
 apiRouter.use('/units', unitRoutes);
-apiRouter.use('/ingredients' ingredientsRoutes,);
+apiRouter.use('/ingredients', ingredientsRoutes);
 module.exports = apiRouter;
