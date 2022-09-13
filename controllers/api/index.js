5f0d4a7bffd01a9a456eb6529a53f72440f573ae
Zachary Warnes <zacharywarnes@gmail.com>
2022-09-12 17:22:51 -0700

    fixing ingredients api route and adding routes to index.js in api folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const apiRouter = require('express').Router();
 const userRoutes = require('./userRoutes');
 const unitRoutes = require('./unitRoutes');
+const ingredientsRoutes= require('./ingredientsRoutes')
 
 apiRouter.use('/users', userRoutes);
 apiRouter.use('/units', unitRoutes);
+apiRouter.use('/ingredients' ingredientsRoutes,);
 module.exports = apiRouter;
